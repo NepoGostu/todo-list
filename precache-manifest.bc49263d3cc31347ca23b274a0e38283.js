85dc1cb2027ca416aed2e2d3d656ccf7de2a9ca3
NepoGostu <andruavanshuls@gmail.com>
2022-10-24 20:35:11 +0300

    Updates

---
+++ precache-manifest.bc49263d3cc31347ca23b274a0e38283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19910be1e80777160f35b0c481afbf44",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7440d8411e5c93b922a8",
+    "url": "/todo-list/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todo-list/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo-list/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7440d8411e5c93b922a8",
+    "url": "/todo-list/static/js/main.02a13310.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
